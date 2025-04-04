7bd9d7deb4770aa3aa53fcafb47121e329442fa2
Marina <fagadarghisa.marina@gmail.com>
2025-04-04 20:36:30 +0300

    Push= base collection page

---
+++ frontend/src/Pages/CollectionPage.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const CollectionPage: React.FC = () =>
+{
+    return(
+        <div>
+            <h1
+                style ={{
+                    fontSize: "32px",
+                    textAlign: "center"
+
+                }}>
+                COLLECTION PAGE
+            </h1>
+        </div>
+    );
+}
+
+export default CollectionPage;
--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -2,6 +2,7 @@ import Login from "./Pages/LoginPage";
 import MainPage from "./Pages/MainPage";
 import { BrowserRouter as Router, Route,Routes} from "react-router-dom";
 import React from "react";
+import CollectionPage from "./Pages/CollectionPage";
 
 
 
@@ -11,6 +12,7 @@ const AppRoutes =() =>{
             <Routes>
                 <Route path="/" element={<Login />} />
                 <Route path="/main" element={<MainPage />} />
+                <Route path="/collection" element={<CollectionPage />} />
             </Routes>
         </Router>
     );
